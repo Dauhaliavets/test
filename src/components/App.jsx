b6bed2049e8e17d82334528c50256af1f3e7f249
Dzmitry Dauhaliavets <dolgolevets21@gmail.com>
2023-08-25 22:39:25 +0300

    fix: minor fixes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,29 @@ import './styles.less';
 import { userData } from '../constants/userData';
 import { Spinner } from './spinner/Spinner';
 import { Loader } from './loader/Loader';
+import { tourmanentDate } from '../constants/tournament';
+import { TournamentItem } from './tournamentItem/TournamentItem';
 
 export const App = () => {
+  const tournamentItems = tourmanentDate.map((item) => <TournamentItem key={item.id} {...item} />);
+
   return (
     <div className='app-container'>
       <h2 className='app-title'>Poker</h2>
+
       <div className='spinner-wrapper'>
         <Spinner />
       </div>
+
       <div className='player-info-wrapper'>
         <PlayerInfo {...userData} />
       </div>
 
-      <Loader />
+      <div className='loader-wrapper'>
+        <Loader />
+      </div>
+
+      <ul className='tournament__list'>{tournamentItems}</ul>
     </div>
   );
 };
